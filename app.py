ea574e46e6c2da9249a1806005fc5217c38d594d
Ayush Kumar <155281851+ayushkumarnitd@users.noreply.github.com>
2026-04-29 00:33:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,71 +3,58 @@ import numpy as np
 import pickle as pk 
 import streamlit as st
 
+# Load the trained model
 model = pk.load(open('model.pkl','rb'))
 
 st.header('Car Price Prediction ML Model')
 
-cars_data = pd.read_csv('Cardetails.csv')
-
-def get_brand_name(car_name):
-    car_name = car_name.split(' ')[0]
-    return car_name.strip()
-
-def clean_data(value):
-    value = str(value).split(' ')[0]
-    value = value.strip()
-    if value == '' or value == 'nan':
-        value = 0
-    return float(value)
-
-cars_data['name'] = cars_data['name'].apply(get_brand_name)
-
-name = st.selectbox('Select Car Brand', cars_data['name'].unique())
+# Lock the select boxes to ONLY the categories the model recognizes
+name = st.selectbox('Select Car Brand', ['Maruti', 'Skoda', 'Honda', 'Hyundai', 'Toyota', 'Ford', 'Renault', 'Mahindra', 'Tata', 'Chevrolet', 'Datsun', 'Jeep', 'Mercedes-Benz', 'Mitsubishi', 'Audi', 'Volkswagen', 'BMW', 'Nissan', 'Lexus', 'Jaguar', 'Land', 'MG', 'Volvo', 'Daewoo', 'Kia', 'Fiat', 'Force', 'Ambassador', 'Ashok', 'Isuzu', 'Opel'])
 year = st.slider('Car Manufactured Year', 1994, 2024)
 km_driven = st.slider('No of kms Driven', 11, 200000)
-fuel = st.selectbox('Fuel type', cars_data['fuel'].unique())
-seller_type = st.selectbox('Seller type', cars_data['seller_type'].unique())
-transmission = st.selectbox('Transmission type', cars_data['transmission'].unique())
-owner = st.selectbox('Owner type', cars_data['owner'].unique())
+fuel = st.selectbox('Fuel type', ['Diesel', 'Petrol', 'LPG', 'CNG'])
+seller_type = st.selectbox('Seller type', ['Individual', 'Dealer', 'Trustmark Dealer'])
+transmission = st.selectbox('Transmission type', ['Manual', 'Automatic'])
+owner = st.selectbox('Owner type', ['First Owner', 'Second Owner', 'Third Owner', 'Fourth & Above Owner', 'Test Drive Car'])
 mileage = st.slider('Car Mileage', 10, 40)
 engine = st.slider('Engine CC', 700, 5000)
 max_power = st.slider('Max Power', 0, 200)
 seats = st.slider('No of Seats', 5, 10)
 
 if st.button("Predict"):
+    # 1. Map text to numbers using Python dictionaries (Much safer than Pandas .replace)
+    name_map = {'Maruti': 1, 'Skoda': 2, 'Honda': 3, 'Hyundai': 4, 'Toyota': 5, 'Ford': 6, 'Renault': 7,
+                'Mahindra': 8, 'Tata': 9, 'Chevrolet': 10, 'Datsun': 11, 'Jeep': 12, 'Mercedes-Benz': 13,
+                'Mitsubishi': 14, 'Audi': 15, 'Volkswagen': 16, 'BMW': 17, 'Nissan': 18, 'Lexus': 19,
+                'Jaguar': 20, 'Land': 21, 'MG': 22, 'Volvo': 23, 'Daewoo': 24, 'Kia': 25, 'Fiat': 26, 
+                'Force': 27, 'Ambassador': 28, 'Ashok': 29, 'Isuzu': 30, 'Opel': 31}
+    
+    fuel_map = {'Diesel': 1, 'Petrol': 2, 'LPG': 3, 'CNG': 4}
+    
+    seller_map = {'Individual': 1, 'Dealer': 2, 'Trustmark Dealer': 3}
+    
+    trans_map = {'Manual': 1, 'Automatic': 2}
+    
+    owner_map = {'First Owner': 1, 'Second Owner': 2, 'Third Owner': 3, 'Fourth & Above Owner': 4, 'Test Drive Car': 5}
+
+    # 2. Convert the user inputs using the maps
+    mapped_name = name_map[name]
+    mapped_fuel = fuel_map[fuel]
+    mapped_seller = seller_map[seller_type]
+    mapped_trans = trans_map[transmission]
+    mapped_owner = owner_map[owner]
+
+    # 3. Create the DataFrame using the converted numbers directly
     input_data_model = pd.DataFrame(
-        [[name, year, km_driven, fuel, seller_type, transmission, owner, mileage, engine, max_power, seats]],
+        [[mapped_name, year, km_driven, mapped_fuel, mapped_seller, mapped_trans, mapped_owner, mileage, engine, max_power, seats]],
         columns=['name','year','km_driven','fuel','seller_type','transmission','owner','mileage','engine','max_power','seats']
     )
 
-    input_data_model['owner'].replace(
-        ['First Owner', 'Second Owner', 'Third Owner', 'Fourth & Above Owner', 'Test Drive Car'],
-        [1, 2, 3, 4, 5], inplace=True)
-
-    input_data_model['fuel'].replace(
-        ['Diesel', 'Petrol', 'LPG', 'CNG'],
-        [1, 2, 3, 4], inplace=True)
-
-    input_data_model['seller_type'].replace(
-        ['Individual', 'Dealer', 'Trustmark Dealer'],
-        [1, 2, 3], inplace=True)
-
-    input_data_model['transmission'].replace(
-        ['Manual', 'Automatic'],
-        [1, 2], inplace=True)
-
-    input_data_model['name'].replace(
-        ['Maruti', 'Skoda', 'Honda', 'Hyundai', 'Toyota', 'Ford', 'Renault',
-         'Mahindra', 'Tata', 'Chevrolet', 'Datsun', 'Jeep', 'Mercedes-Benz',
-         'Mitsubishi', 'Audi', 'Volkswagen', 'BMW', 'Nissan', 'Lexus',
-         'Jaguar', 'Land', 'MG', 'Volvo', 'Daewoo', 'Kia', 'Fiat', 'Force',
-         'Ambassador', 'Ashok', 'Isuzu', 'Opel'],
-        [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31],
-        inplace=True)
-
-    # Convert all columns to float to avoid type errors
+    # 4. Convert all columns to float (Since they are all numbers now, this will never fail!)
     input_data_model = input_data_model.astype(float)
 
+    # 5. Run the prediction
     car_price = model.predict(input_data_model)
 
+    # 6. Display the result
     st.success('Estimated Car Price: ₹ ' + str(round(car_price[0], 2)))
